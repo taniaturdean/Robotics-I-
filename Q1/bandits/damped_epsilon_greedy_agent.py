3f86f321bd46cdd296917de047a65fa960351e27
Kaloyan Rusev <kaloyan.rusev.19@ucl.ac.uk>
2022-02-27 15:04:52 +0000

    here you are Tania

--- Q1/bandits/damped_epsilon_greedy_agent.py
+++ Q1/bandits/damped_epsilon_greedy_agent.py
@@ -10,7 +10,7 @@ from .agent import Agent
 
 
 class DampedEpsilonGreedyAgent(Agent):
-    
+
     def __init__(self, environment, epsilon):
         super().__init__(environment)
         self._epsilon = epsilon
@@ -22,13 +22,8 @@ class DampedEpsilonGreedyAgent(Agent):
             average_q = np.divide(self.total_reward, self.number_of_pulls)
             best_action = np.where(average_q == np.amax(average_q))[0]
             action = best_action[0]
-            
+
         return action
-    
-    # Q5.c:
-    # Explore with different values for this function and see what happens.
+
     def _compute_epsilon(self):
-        
-        return self._epsilon * math.exp(-1 * self.total_number_of_pulls)
-            
-        
+        return self._epsilon * math.exp(-0.05 * self.total_number_of_pulls)
